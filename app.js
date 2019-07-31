db41e9abc2adf86747ee75dfabe7bf95f8031050
Eduardo Jimenez <ed.jv9260@gmail.com>
2019-07-31 11:37:21 -0600

    Commit message

---
+++ app.js
@@ -0,0 +1,8 @@
+var contenido = document.querySelector('#contenido')
+function traer() {
+    fetch('texto.txt')
+    .then(data => data.text())
+    .then(data => {
+        console.log(data )
+    })
+}
